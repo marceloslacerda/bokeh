8183dfb9c4243549ae392ab1c35fadd74c72c7f8
Ian Thomas <ianthomas23@gmail.com>
2023-07-20 09:32:11 +0100

    Include model id in render console messages

--- bokehjs/src/lib/models/plots/plot_canvas.ts
+++ bokehjs/src/lib/models/plots/plot_canvas.ts
@@ -833,7 +833,7 @@ export class PlotView extends LayoutDOMView implements Renderable {
   protected _actual_paint(): void {
 
     //////////////// NEW CODE
-    console.log("PlotView._actual_paint", this._render_count, "start")
+    console.log("PlotView._actual_paint", this.model.id, this._render_count, "start")
 
     const {document} = this.model
     if (document != null) {
@@ -913,7 +913,7 @@ export class PlotView extends LayoutDOMView implements Renderable {
     this._needs_paint = false
 
     //////////////// NEW CODE
-    console.log("PlotView._actual_paint", this._render_count, "end")
+    console.log("PlotView._actual_paint", this.model.id, this._render_count, "end")
     this._render_count++
   }
 
